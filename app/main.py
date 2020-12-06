91b232765d33ebe720750051a01dc0d4d41a3763
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2020-12-07 04:54:47 +0900

    (wip): リファクタリング

--- app/main.py
+++ app/main.py
@@ -42,20 +42,21 @@ def _make_feature(row: pd.Series, debug=True):
         properties = OrderedDict(row) if debug \
             else OrderedDict({k: v for k, v in OrderedDict(row).items() if not k.startswith('_') })
 
-        # lat, lngは「properitesではなくgeometryに配置されるため
+        # lat, lngは「properites」ではなく「geometry」に配置される
         lat = properties.pop('lat')
         lng = properties.pop('lng')
 
         # その他の補足情報を追加
-        gmap_args = properties['normalized_address'] + ' ' + properties['shop_name']
-        properties['GoogleMap'] = 'https://www.google.com/maps/search/?q=' + quote(gmap_args)
+        googlemap_q_string = '{} {}'.format(properties['normalized_address'], properties['shop_name'])
+        properties['GoogleMap'] = 'https://www.google.com/maps/search/?q=' + quote(googlemap_q_string)
         if debug:
             properties['_国土地理院地図のURL'] = f'https://maps.gsi.go.jp/#17/{lat}/{lng}/'
     except Exception as e:
         logger.error(properties)
         raise e
 
-    return Feature(geometry=Point((lng, lat)), properties=properties)
+    coords = (lng, lat) # lng, latの順番に注意
+    return Feature(geometry=Point(coords), properties=properties)
 
 
 def _normalize(row: pd.Series):
@@ -79,7 +80,8 @@ def _normalize(row: pd.Series):
         row['_ERROR'] = np.nan
 
     except (NormalizeError, GeocodeError) as e:
-        # 例外(NormalizeError, GeocodeError)が発生した場合
+        # 住所の正規化エラー(NormalizeError), ジオコーディングエラー(GeocodeError)が発生した場合
+        # dfの_ERROR列に発生したエラー名を追加、後から追えるようにしておく
         name = e.__class__.__name__
         row['_ERROR'] = name
         logger.warning('{}: {}'.format(name, row.to_dict()))
@@ -101,10 +103,10 @@ def write_geojson(df: pd.DataFrame, outfile: str, debug=True):
     feature_collection = FeatureCollection(features=features)
     with open(outfile, 'w', encoding='utf-8') as f:
         if debug:
-            # デバッグ用GeoJsonはpretty
+            # デバッグ用GeoJsonはprettyして出力
             json.dump(feature_collection, f, ensure_ascii=False, indent=4)
         else:
-            # 本番用GeoJsonはファイルサイズ削減のためインデントと改行なし
+            # 本番用GeoJsonは転送量削減のため、インデントと改行なしで出力
             json.dump(feature_collection, f, ensure_ascii=False, separators=(',', ':'))
 
 
@@ -112,36 +114,37 @@ def main(base: str, cleanup=True):
     # CSV読み込み
     logger.info(f'base={base}')
     infile = pathlib.Path.cwd() / f'../data/csv/{base}.csv'
-    df = pd.read_csv(infile, encoding="utf-8", dtype={'shop_name': str, 'tel': str}).fillna('')
+
+    # df = pd.read_csv(infile, encoding="utf-8", dtype={'shop_name': str, 'tel': str, 'zip_code': str}).fillna('')
+
+    # CSVの全カラムを文字列として読み込む
+    # MEMO: データ元のサイト依存だが、書式によってはtel, zip_codeなどがintと認識されるため
+    # 基本的にクローラー側では書式変換はせず、(必要であれば)本csv2geojson側で書式変換を行う方針
+    df = pd.read_csv(infile, encoding="utf-8", dtype=str).fillna('')
 
     # 読み込んだCSVデータの重複レコードチェック(店名 and 住所)
-    # 店名、住所を個別で実行することも可能だが、以下の理由で店名、住所は単体だと重複判定できない場合がある
+    # 入力データチェックとして十分とは言えないが、参考程度に
+    # MEMO: 以下のパターンがあるので、店名および住所単体だと重複判定できない。
     # ・同じ店名の別の店(例: 愛知県の「すずや」)
-    # ・同じ住所(例: 同じショッピングモール内)にある別の店
-    # 入力ミスのパターンによっては十分とは言えないが、参考程度にチェックを入れておく
+    # ・同じ住所の別の店(例: 同じショッピングモール内)
     duplicated_records = df[df.duplicated(subset=['shop_name', 'address'])]
     if not duplicated_records.empty:
         logger.warning('以下のレコードが重複しています')
         logger.warning(duplicated_records)
-        # 重複行を消す
+        # 重複行の削除
         df.drop_duplicates(subset=['shop_name', 'address'], keep='last', inplace=True)
 
     # normalized_csvの作成
     # ・カテゴリ名
     # ・住所
-    # 上記２つを正規化し、さらにデバッグ用情報を付与したもの
+    # 上記を正規化し、さらにデバッグ用の情報(主にlatlng関係)を付与したもの
     outfile = pathlib.Path.cwd() / f'../data/normalized_csv/{base}.csv'
     logger.info(f'create normalized_csv... > {outfile}')
     df = df.apply(_normalize, axis=1)
 
-    # 正規化エラーになっているレコードの内容を {base}.error.txt として保存
-    error_df = df[df['_ERROR'].notnull()]
-    if not error_df.empty:
-        filename = outfile.parent.parent / 'error' / (outfile.name + '.error.txt')
-        error_df.to_json(filename, orient='records', lines=True, force_ascii=False)
-
-    # それ以外のレコードをnormalized_csvとして保存(_ERROR列は削除)
-    df = df[df['_ERROR'].isnull()].drop(columns='_ERROR')
+    # エラー以外のレコードをnormalized_csvとして保存
+    error_df = df[df['_ERROR'].notnull()]                   # エラーレコード(_ERROR列に値を含む)を分離
+    df = df[df['_ERROR'].isnull()].drop(columns='_ERROR')   # エラーレコード以外を取得し、_ERROR列は削除
     df['genre_code'] = df['genre_code'].astype(int)
     df.to_csv(outfile, columns=NORMALIZED_CSV_ORDER, index=False)
     logger.info(f'success.')
@@ -163,10 +166,15 @@ def main(base: str, cleanup=True):
         write_geojson(df[df['genre_code'] == genre_code], outfile=output_dir / geojson_name, debug=False)
         write_geojson(df[df['genre_code'] == genre_code], outfile=output_dir_debug / geojson_name,)
 
+    # エラーレコードがあれば、エラー確認用jsonを出力
+    if not error_df.empty:
+        error_df.to_json(output_dir / '_error.json', \
+            orient='records', lines=True, force_ascii=False)
+
 
 if __name__ == "__main__":
     # usage:
-    # $ docker-compose run csv2geojson python /app/main.py yamanashi
+    # $ docker-compose run csv2geojson python /app/main.py --base yamanashi
 
     # GeoJSONのFeatureを作成するサンプル
     # addressからジオコーディングでlat,lngを取得
@@ -186,14 +194,23 @@ if __name__ == "__main__":
     assert feature.geometry.type == 'Point'
     assert feature.geometry.coordinates == [139.465439, 36.301109], "latlng did not match."
 
-    # TODO: もう少し真面目にやってもよい(後述の全件処理と一緒に)
+    # TODO: もう少し真面目に、かつクラスにしてあげたい
+    # argsもinとoutをそれぞれ指定してあげれば良さそう
+    # 例: --infile data/infile/tochigi.csv --outdir data/outfile/tochigi/　相当
+    # - data/infile
+    #   - tochigi.csv
+    # - data/outfile
+    #   - tochigi/normalized.csv
+    #   - tochigi/genre[1-10].geojson
+    #   - tochigi/_error.json
+    #   - tochigi/_debug/genre[1-10].geojson
     parser = argparse.ArgumentParser(description='goto-eat-csv2geojson')
-    parser.add_argument('base', help='e.g.: 09_tochigi')
+    parser.add_argument('--base', help='例: tochigi')   # 個別指定する場合のオプション
     args = parser.parse_args()
     base = args.base
 
     # やる気のない全件処理
-    if base == 'all':
+    if not base:
         target = pathlib.Path.cwd() / f'../data/csv/'
         for csv in list(target.glob('*.csv')):
             main(csv.stem)
